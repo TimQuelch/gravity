9fbb286c61aa2f3fbe1758a30a7ef51860e65f95
Tim Quelch <tim@quelch.name>
2017-03-13 11:44:45 +1000

    Add Doxygen documentation comments

--- include/simulation.h
+++ include/simulation.h
@@ -1,11 +1,35 @@
+/**
+ * @file simulation.h
+ * @brief Functions to run n-body simulation
+ * @author Tim Quelch
+ */
+
 #ifndef GRAVITY_SIMULATION_H
 #define GRAVITY_SIMULATION_H
 
 namespace gravity {
+	/**
+	 * @brief Initialise particles to random positions
+	 *
+	 * @param num_particles The number of particles
+	 */
 	void init_particles(int num_particles);
+
+	/**
+	 * @brief Run the simulation
+	 *
+	 * @param num_timesteps Number of timesteps to run
+	 */
 	void run_simulation(int num_timesteps);
 
+	/**
+	 * @brief Attract all particles to each other
+	 */
 	void attract_particles();
+
+	/**
+	 * @brief Step all particles according to their velocity vector
+	 */
 	void step_particles();
 }
 
