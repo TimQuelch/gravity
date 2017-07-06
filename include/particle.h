ce5a702380aaf1a7616597e68e8242b2b510f0da
Tim Quelch <tim@quelch.name>
2017-07-06 23:38:02 +1000

    Use C++ style documentation comments

--- include/particle.h
+++ include/particle.h
@@ -1,8 +1,6 @@
-/**
- * \file particle.h
- * \brief Particle and vector classes for n-body simulation
- * \author Tim Quelch
- */
+/// \file particle.h
+/// Particle and vector classes for n-body simulation
+/// \author Tim Quelch
 
 #ifndef GRAVITY_PARTICLE_H
 #define GRAVITY_PARTICLE_H
@@ -11,81 +9,61 @@
 #include <stdexcept>
 
 namespace gravity {
-	/**
-	 * \brief Gravitational constant. Gravitational forces are proportional to this constant
-	 */
+	/// Gravitational constant. Gravitational forces are proportional to this constant
 	const float gravitationalConstant = 0.10;
 	// const float gravitationalConstant = 6.674e-11;
 
-	/**
-	 * \brief Simple 3D vector of floats
-	 */
+	/// Simple 3D vector of floats
 	struct Vec3 {
-		float x{0}; /**< \brief x component of vector */
-		float y{0}; /**< \brief y component of vector */
-		float z{0}; /**< \brief z component of vector */
+		float x{0}; ///< x component of vector
+		float y{0}; ///< y component of vector
+		float z{0}; ///< z component of vector
 
-		Vec3() = default; /**< \brief Create vector with all components as 0 */
+		Vec3() = default; ///< Create vector with all components as 0
 
-		/**
-		 * \brief Create vector with initial values
-		 * \param x Initial x component
-		 * \param y Initial y component
-		 * \param z Initial z component
-		 */
+		/// Create vector with initial values
+		/// \param x Initial x component
+		/// \param y Initial y component
+		/// \param z Initial z component
 		Vec3(float x, float y, float z)
 		    : x{x}
 		    , y{y}
 		    , z{z} {}
 
-		/**
-		 * \brief Calculate the magnitude of the vector
-		 * \return The magnitude of the vector
-		 */
+		/// Calculate the magnitude of the vector
+		/// \return The magnitude of the vector
 		float magnitude() const { return std::sqrt(x * x + y * y + z * z); }
 
-		/**
-		 * \brief Calculate the unit vector of the vector
-		 * \return The unit vector
-		 */
+		/// Calculate the unit vector of the vector
+		/// \return The unit vector
 		Vec3 direction() const { return *this * (1 / magnitude()); }
 
-		/**
-		 * \brief Add vector to this vector
-		 * \param other Another vector
-		 * \return *this
-		 */
+		/// Add vector to this vector
+		/// \param other Another vector
+		/// \return *this
 		Vec3& operator+=(const Vec3& other);
 
-		/**
-		 * \brief Add two vectors together
-		 * \param one A vector
-		 * \param two Another vector
-		 * \return The sum of the two vectors
-		 */
+		/// Add two vectors together
+		/// \param one A vector
+		/// \param two Another vector
+		/// \return The sum of the two vectors
 		friend Vec3 operator+(const Vec3& one, const Vec3& two) { return Vec3{one} += two; }
 
-		/**
-		 * \brief Subtract vector from this vector
-		 * \param other Another vector
-		 * \return *this
-		 */
+		/// Subtract vector from this vector
+		/// \param other Another vector
+		/// \return *this
 		Vec3& operator-=(const Vec3& other);
 
-		/**
-		 * \brief Subtract a vector from another
-		 * \param one A vector
-		 * \param two Another vector
-		 * \return The difference of the two vectors
-		 */
+		/// Subtract a vector from another
+		/// \param one A vector
+		/// \param two Another vector
+		/// \return The difference of the two vectors
 		friend Vec3 operator-(const Vec3& one, const Vec3& two) { return Vec3{one} -= two; }
 
-		/**
-		 * \brief Multiply the vector by a scalar
-		 * \tparam A numeric scalar type
-		 * \param scalar The scalar value
-		 * \return *this
-		 */
+		/// Multiply the vector by a scalar
+		/// \tparam A numeric scalar type
+		/// \param scalar The scalar value
+		/// \return *this
 		template <typename Scalar>
 		Vec3& operator*=(Scalar scalar) {
 			x *= scalar;
@@ -94,115 +72,84 @@ namespace gravity {
 			return *this;
 		}
 
-		/**
-		 * \brief Multiply a vector by a scalar
-		 * \tparam A numeric scalar type
-		 * \param scalar The scalar value
-		 * \param vec The vector
-		 * \return The scaled vector
-		 */
+		/// Multiply a vector by a scalar
+		/// \tparam A numeric scalar type
+		/// \param scalar The scalar value
+		/// \param vec The vector
+		/// \return The scaled vector
 		template <typename Scalar>
 		friend Vec3 operator*(Scalar scalar, const Vec3& vec) {
 			return Vec3{vec} *= scalar;
 		}
 
-		/**
-		 * \brief Multiply a vector by a scalar
-		 * \tparam A numeric scalar type
-		 * \param scalar The scalar value
-		 * \param vec The vector
-		 * \return The scaled vector
-		 */
+		/// \copydoc Vec3::operator*
 		template <typename Scalar>
 		friend Vec3 operator*(const Vec3& vec, Scalar scalar) {
 			return Vec3{vec} *= scalar;
 		}
 	};
 
-	/**
-	 * \brief A single particle in a n-body simulation
-	 * Particles have a position, velocity, and a mass. Momentum can be found from these properties.
-	 * Particles atract each other through gravity, and can collide with other particles
-	 */
+	/// A single particle in a n-body simulation
+	/// Particles have a position, velocity, and a mass. Momentum can be found from these
+	/// properties.
+	/// Particles atract each other through gravity, and can collide with other particles
 	class Particle {
 	public:
-		/**
-		 * \brief Create particle with default values
-		 * Position and velocity are 0. Mass is 1
-		 */
+		/// Create particle with default values
+		/// Position and velocity are 0. Mass is 1
 		Particle() = default;
 
-		/**
-		 * \brief Create particle with an initial position and velocity
-		 * \param position The initial position of the particle
-		 * \param velocity The initial velocity of the particle
-		 * \param mass The mass of the particle. Must be positive
-		 */
+		/// Create particle with an initial position and velocity
+		/// \param position The initial position of the particle
+		/// \param velocity The initial velocity of the particle
+		/// \param mass The mass of the particle. Must be positive
 		Particle(Vec3 position, Vec3 velocity, float mass)
 		    : pos_{position}
 		    , vel_{velocity}
 		    , mass_{mass > 0 ? mass : throw std::invalid_argument{"Mass must be positive"}}
 		    , radius_{computeRadius()} {}
 
-		/**
-		 * \brief Gets the position of the particle
-		 * \return The position of the particle
-		 */
+		/// Gets the position of the particle
+		/// \return The position of the particle
 		Vec3 pos() const { return pos_; }
 
-		/**
-		 * \brief Gets the velocity of the particle
-		 * \return The velocity of the particle
-		 */
+		/// Gets the velocity of the particle
+		/// \return The velocity of the particle
 		Vec3 vel() const { return vel_; }
 
-		/**
-		 * \brief Gets the momentum of the particle. Momentum = mass * velocity
-		 * \return The momentum of the particle
-		 */
+		/// Gets the momentum of the particle. Momentum = mass * velocity
+		/// \return The momentum of the particle
 		Vec3 momentum() const { return vel_ * mass_; }
 
-		/**
-		 * \brief Gets the mass of the particle
-		 * \return The mass of the particle
-		 */
+		/// Gets the mass of the particle
+		/// \return The mass of the particle
 		float mass() const { return mass_; }
 
-		/**
-		 * \brief Gets the radius of the particle
-		 * The radius of the particle depends on the mass. Radius should be updated each time the
-		 * mass of the particle changes.
-		 * \return The radius of the particle
-		 */
+		/// Gets the radius of the particle
+		/// The radius of the particle depends on the mass. Radius should be updated each time the
+		/// mass of the particle changes.
+		/// \return The radius of the particle
 		float radius() const { return radius_; }
 
-		/**
-		 * \brief Steps the particle in the direction of the velocity vector
-		 */
+		/// Steps the particle in the direction of the velocity vector
 		void step();
 
-		/**
-		 * \brief Attracts a particle to another particle by gravity.
-		 * This modifies the velocity of the particle, but not the position
-		 * \param other Another particle
-		 */
+		/// Attracts a particle to another particle by gravity.
+		/// This modifies the velocity of the particle, but not the position
+		/// \param other Another particle
 		void attract(const Particle& other);
 
-		/**
-		 * \brief Merge two particles together when they collide.
-		 * New particle is created conserving momentum.
-		 * \param one A particle
-		 * \param two Another particle
-		 * \return The particle that is the result of the collision
-		 */
+		/// Merge two particles together when they collide.
+		/// New particle is created conserving momentum.
+		/// \param one A particle
+		/// \param two Another particle
+		/// \return The particle that is the result of the collision
 		static Particle collide(const Particle& one, const Particle& two);
 
-		/**
-		 * \brief Checks whether two particles are colliding
-		 * \param one A particle
-		 * \param two Another particle
-		 * \return True if the particles' radii are overlapping
-		 */
+		/// Checks whether two particles are colliding
+		/// \param one A particle
+		/// \param two Another particle
+		/// \return True if the particles' radii are overlapping
 		static bool checkCollision(const Particle& one, const Particle& two);
 
 	private:
@@ -211,12 +158,10 @@ namespace gravity {
 		float mass_{1};
 		float radius_{computeRadius()};
 
-		/**
-		 * \brief Compute the radius of the particle
-		 * This should be used to update the radius of the particle any time the mass is changed.
-		 * The value is derived from the volume of a sphere, V = 4pi/3 r^3. It is assumed that
-		 * density is normalised.
-		 */
+		/// Compute the radius of the particle
+		/// This should be used to update the radius of the particle any time the mass is changed.
+		/// The value is derived from the volume of a sphere, V = 4pi/3 r^3. It is assumed that
+		/// density is normalised.
 		float computeRadius() const { return std::cbrt((3 * mass_) / (4 * M_PI)); }
 	};
 }
--- include/simulation.h
+++ include/simulation.h
@@ -1,41 +1,29 @@
-/**
- * \file simulation.h
- * \brief Functions to run n-body simulation
- * \author Tim Quelch
- */
+/// \file simulation.h
+/// Functions to run n-body simulation
+/// \author Tim Quelch
 
 #ifndef GRAVITY_SIMULATION_H
 #define GRAVITY_SIMULATION_H
 
 namespace gravity {
-	/**
-	 * \brief Initialise particles to random positions
-	 * \param numParticles The number of particles
-	 */
+	/// Initialise particles to random positions
+	/// \param numParticles The number of particles
 	void initParticles(int numParticles);
 
-	/**
-	 * \brief Run the simulation
-	 * \param numTimesteps Number of timesteps to run
-	 */
+	/// Run the simulation
+	/// \param numTimesteps Number of timesteps to run
 	void runSimulation(int numTimesteps);
 
-	/**
-	 * \brief Attract all particles to each other
-	 */
+	/// Attract all particles to each other
 	void attractParticles();
 
-	/**
-	 * \brief Collide nearby particles
-	 * Particles whose radii overlap will be collided, merging into a single particle. One of the
-	 * particles in a collision has it's mass and velocity updated to conserve momentum, and the
-	 * other is removed from the simulation
-	 */
+	/// Collide nearby particles
+	/// Particles whose radii overlap will be collided, merging into a single particle. One of the
+	/// particles in a collision has it's mass and velocity updated to conserve momentum, and the
+	/// other is removed from the simulation
 	void collideParticles();
 
-	/**
-	 * \brief Step all particles according to their velocity vector
-	 */
+	/// Step all particles according to their velocity vector
 	void stepParticles();
 }
 
